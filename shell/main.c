bb671f0bdc1dccebe5a0aa5d74895cbae9ad430d
Igor Bianchi <igorbianchi_@hotmail.com>
2018-06-18 11:21:15 -0300

    Pipeline execution implemented with IO redirection

--- shell/main.c
+++ shell/main.c
@@ -1,7 +1,7 @@
 /* main.c
 Copyright (c) 2018,
 
-Igor Guilherme Bianchi
+Igor Guilherme Bianchi   igorgbianchi@gmail.com
 Lucas Martins Macedo
 Marco Alcassio Uski			uski.marco@gmail.com
 Willian Kaminobo Takaezu		takaezu.ufscar@gmail.com
@@ -22,87 +22,99 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
+#include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include "tparse.h"
-#include "debug.h"
+#include <assert.h>
 #include <unistd.h>
-#include <stdbool.h>
 #include <sys/wait.h>
 #include <fcntl.h>
-
-#define PROMPT "$"
+#include <string.h>
+#include <errno.h>
+#include "functions.h"
 
 int main(int argc, char **argv) {
-    int aux;
-    buffer_t *commandLine = new_command_line();
-    pipeline_t *pipeline = new_pipeline();
+    buffer_t *command_line;
+    int aux, pid = 0;
+
+    pipeline_t *pipeline;
+    command_line = new_command_line();
+    pipeline = new_pipeline();
+
+    /* Set shell to ingore most signals */
+    set_signals(SIG_IGN);
 
     while (true) {
-        /* Prompt */
+        /* Prompt. */
         printf("%s ", PROMPT);
         fflush(stdout);
-        aux = read_command_line(commandLine);
-        sysfatal(aux < 0);
+        aux = read_command_line(command_line);
+        if (aux == 0) {
+            break;
+        }
+        assert (aux > 0);
 
-        if (!parse_command_line(commandLine, pipeline) && pipeline->ncommands > 0) {
-            int ret;
-            int prevOut = dup(1);
-            int prevIn = dup(0);
-            /*TODO error redirection*/
+        /* Parse */
+        if (!parse_command_line(command_line, pipeline)) {
+            int pipes[2];
+            int in_file = STDIN_FILENO;
+            int out_file = STDOUT_FILENO;
+            int i;
 
-            if (pipeline->ncommands > 1) {
-                printf("Hold the door! Pipes are not yet available in this version.\n"
-                       "Running only the first command from the pipeline.");
+            if (pipeline->ncommands > 0 && strcmp("exit", pipeline->command[0][0]) == 0) {
+                /* exit builtin to leave the shell */
+                break;
             }
 
-            if (REDIRECT_STDOUT(pipeline)) {
-                int out;
-
-                out = open(pipeline->file_out, O_RDWR | O_CREAT | O_TRUNC, 0664);
-                if (out != -1) {
-                    dup2(out, 1);
-                    close(out);
-                } else {
-                    debug(true && "open failed", "Couldn\'t open file for output redirection");
-                }
-            }
-
+            /* change input file */
             if (REDIRECT_STDIN(pipeline)) {
-                int in;
+                in_file = open(pipeline->file_in, O_RDONLY);
+                assert(in_file != -1);
+            }
 
-                in = open(pipeline->file_in, O_RDONLY);
-                if (in != -1) {
-                    dup2(in, 0);
-                    close(in);
+            for (i = 0; i < pipeline->ncommands; i++) {
+                if (i == pipeline->ncommands - 1) { /* is last process? */
+                    /* change output file */
+                    out_file = STDOUT_FILENO;
+                    if (REDIRECT_STDOUT(pipeline)) {
+                        out_file = open(pipeline->file_out, O_RDWR | O_CREAT | O_TRUNC, 0664);
+                        assert(out_file != -1);
+                    }
                 } else {
-                    debug(true && "open failed", "Couldn\'t open file for input redirection");
+                    /* pipe between processes */
+                    assert(pipe(pipes) == 0);
+                    out_file = pipes[1];
                 }
-            }
 
-            ret = fork();
-            if (ret > 0) {
-                /* Parent process */
-                int status; /* <- Use this for child information */
-                waitpid(ret, &status, 0);
-                dup2(1, prevOut);
-                dup2(0, prevIn);
-            } else if (ret == 0) {
-                /* Child process  */
-                execvp(pipeline->command[0][0], pipeline->command[0]);
+                pid = launch(pipeline->command[i], in_file, out_file);
 
-                /* Error on changing process image */
-                sysfatal("execvp failed"); /* Fail for now */
-            } else {
-                /* Error on fork */
-                sysfatal("fork failed");
+                /* clear already redirected IO */
+                if (in_file != STDIN_FILENO) {
+                    close(in_file);
+                }
+                if (out_file != STDOUT_FILENO) {
+                    close(out_file);
+                }
+
+                /* next input is piped from this output */
+                in_file = pipes[0];
             }
+        }
+
+        /* Parent */
+        if (RUN_FOREGROUND(pipeline)) {
+            /* Wait for the last procces of the pipeline */
+            int status;
+            waitpid(pid, &status, 0);
+        } else {
+            printf("Background job control not yet implemented\n");
+            fflush(stdout);
         }
     }
 
-    release_command_line(commandLine);
+    release_command_line(command_line);
     release_pipeline(pipeline);
-
 
     return EXIT_SUCCESS;
 }
